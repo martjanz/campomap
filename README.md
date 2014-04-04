4f4baeecd722c7fb8eaf9001594265a515982572
Martjanz <martin.anzorena@gmail.com>
2014-04-04 01:16:28 -0300

    Primera versión usable.
    Algo es algo.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-campomap
+﻿CampoMap
 ========
+Los datos están, hay que usarlos.
 
-Los datos están. Hay que usarlos.
+Objetivo
+--------
+Este es un proyecto, nacido y criado en mi tiempo libre, que surge de la necesidad de combinar datos de cartografía censal con capas de servicios web de mosaicos geográficos descriptivos (OpenStreetMap) y/o satelitales (Google Maps Satellital, Bing Maps, etc).
+
+Fuentes de datos
+----------------
+La cartografía censal 2010 de Buenos Aires y Ciudad Aut—noma de Buenos Aires est‡ publicada en los sitios web de las respectivas Direcciones Provinciales de Estadística.
+
+La cartografía de OpenStreetMap es pública, libre y comunitaria. Puede descargarse como archivo para ser utilizada en un servidor de base de datos local PostgreSQL con extensión geográfica PostGIS.
+
+Librerías
+---------
+* jQuery 2.1.0
+* Leaflet 0.7
+* Leaflet-plugins 1.1 (https://github.com/shramov/leaflet-plugins)
+* Bootstrap 3.1.1
+
+== Base de datos ==
+PostgreSQL (con extensión PostGIS)
---
+++ js/map.js
@@ -0,0 +1,81 @@
+var map = "";
+
+function initMap() {
+	// OpenStreetMap VM - Sólo tiles Argentina
+	/*var osm_local = new L.tileLayer('http://localhost:8001/osm_tiles/{z}/{x}/{y}.png', {
+	  attribution: 'Mapa base &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Licencia <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imágenes © <a href="http://cloudmade.com">CloudMade</a>',
+	  maxZoom: 18
+	}).addTo(map);*/
+
+	// OpenStreetMap VM (acceso desde intranet) - Sólo tiles Argentina
+	var osm_intranet = new L.tileLayer('http://172.16.48.68:8001/osm_tiles/{z}/{x}/{y}.png', {
+	  attribution: 'Mapa base &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Licencia <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imágenes © <a href="http://cloudmade.com">CloudMade</a>',
+	  maxZoom: 18
+	});
+
+	var osm_remote = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	  attribution: 'Mapa base &copy; Contribuyentes de <a href="http://openstreetmap.org">OpenStreetMap</a>, Licencia <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	  maxZoom: 18
+	});
+
+	var google_satellite = new L.Google();
+	var google_terrain = new L.Google('TERRAIN');
+	var bing = new L.BingLayer("AsOWvxBlrK6QzYW2gOFne8C4Ln6OgehGD85BeFZIH-P6s5uIv1rjoftPqjGjZHgd");
+
+	var baseLayers = {
+	  "OpenStreetMap Internet": osm_remote,
+	  "OpenStreetMap Intranet": osm_intranet,
+	  "Bing Maps": bing,
+	  "Google Satellite": google_satellite,
+	  "Google Terrain": google_terrain
+	};
+
+	/**
+	 * Provincia de Buenos Aires
+	 *
+	 * == Puntos máximos ==
+	 * Máx N -33.2677/-60.2656
+	 * Max S -41.0373/-62.7340
+	 * Max E -36.8763/-56.6662
+	 * Max W -37.8415/-63.3805
+	 *
+	 * == Caja == 
+	 *  NE -33.2677/-56.6662
+	 *  SW -41.0373/-63.3805
+	 *
+	 **/
+
+	/* Carga mapa con layer por defecto. */
+	map = L.map("map", {
+	  layers: [osm_remote],
+	  maxBounds: [ // Caja dentro de la que se podrá mover la vista.
+	  	[-33, -56.5], // Extremo Noreste
+	  	[-41.04, -63.5]  // Extremo Sudoeste
+	  ]
+	});
+
+	var layerControl = L.control.layers(baseLayers).addTo(map);
+
+	/*map.addControl(new L.Control.Layers({'OSM Localhost': osm_local, 'OSM Intranet': osm_intranet, 'Google':ggl, 'Google Terrain':ggl2, 'Bing': bing, 'OSM Internet': osm_internet}, {}));*/
+	//map.addControl(new L.Control.Layers({'OSM Internet': osmRemoteTiles}, {}));
+
+	map.fitBounds([[-33, -56.5], [-41.04, -63.5]]);
+}
+
+/**
+ * function mostrarRadio()
+ *
+ * Carga y muestra en mapa radio censal que recibe por parámetro.
+ **/
+function mostrarRadio(input) {
+	var radiosLayer = L.geoJson(input);
+
+	/* Agrega popup*/
+	radiosLayer.bindPopup("Humilde y aleatorio radio censal de prueba.");
+
+	/* Ajusta mapa a layer radio */
+	map.fitBounds(radiosLayer.getBounds(), {duration: 1, animate: true});
+
+	/* Muestra layer radio */
+	radiosLayer.addTo(map);
+}
